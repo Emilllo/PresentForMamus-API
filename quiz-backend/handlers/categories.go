cb0daa30019d7474fd53c103e95526f40797bd8a
R1lty <tiv51tiv51@gmail.com>
2026-05-08 15:45:28 +0300

    category

---
+++ quiz-backend/handlers/categories.go
@@ -0,0 +1,93 @@
+package handlers
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+
+	"quiz-backend/db"
+	"quiz-backend/models"
+)
+
+func CategoriesHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		GetCategories(w, r)
+
+	case http.MethodPost:
+		CreateCategory(w, r)
+
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func GetCategories(w http.ResponseWriter, r *http.Request) {
+	rows, err := db.DB.Query(
+		context.Background(),
+		`
+		SELECT id, name
+		FROM categories
+		ORDER BY name ASC
+	`,
+	)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	defer rows.Close()
+
+	categories := []models.Categories{}
+
+	for rows.Next() {
+		var category models.Categories	
+
+		err := rows.Scan(
+			&category.ID,
+			&category.Name,
+		)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		categories = append(categories, category)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(categories)
+}
+
+func CreateCategory(w http.ResponseWriter, r *http.Request) {
+	var category models.Categories
+
+	err := json.NewDecoder(r.Body).Decode(&category)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = db.DB.QueryRow(
+		context.Background(),
+		`
+		INSERT INTO categories (name)
+		VALUES ($1)
+		RETURNING id
+		`,
+		category.Name,
+	).Scan(&category.ID)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	json.NewEncoder(w).Encode(category)
+}
